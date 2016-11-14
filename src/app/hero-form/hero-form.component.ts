453ee90e3dd2a7b8219f2c71d8c150d475c969b5
Neo <neolwc@gmail.com>
2016-11-14 12:10:36 -0500

    Two-way data binding with ngModel

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -16,7 +16,4 @@ export class HeroFormComponent {
   submitted = false;
 
   onSubmit() { this.submitted = true; }
-
-  // TODO: Remove this when we're done
-  get diagnostic() { return JSON.stringify(this.model); }
 }
