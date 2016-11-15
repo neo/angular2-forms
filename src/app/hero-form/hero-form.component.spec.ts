02d3ea0ad2c505f9c1e0424a3a03f18c0d543c9b
Neo <neolwc@gmail.com>
2016-11-15 12:11:45 -0500

    Update tests

--- src/app/hero-form/hero-form.component.spec.ts
+++ src/app/hero-form/hero-form.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
 import { HeroFormComponent } from './hero-form.component';
 
 describe('HeroFormComponent', () => {
@@ -11,6 +12,7 @@ describe('HeroFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ FormsModule ],
       declarations: [ HeroFormComponent ]
     })
     .compileComponents();
